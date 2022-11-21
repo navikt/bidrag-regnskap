9b87ab86919aca5a43e837820c5a974140025bd9
Simen Høston <simen.naper.hoston@webstep.no>
2022-11-21 13:47:24 +0100

    Enda bedre logging ved feilhåndteringer

--- src/main/kotlin/no/nav/bidrag/regnskap/aop/DefaultRestControllerAdvice.kt
+++ src/main/kotlin/no/nav/bidrag/regnskap/aop/DefaultRestControllerAdvice.kt
@@ -22,7 +22,7 @@ class DefaultRestControllerAdvice {
   fun handleOtherExceptions(exception: Exception): ResponseEntity<*> {
     LOGGER.warn("Det skjedde en ukjent feil: ${exception.message}", exception)
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-      .header(HttpHeaders.WARNING, "Det skjedde en ukjent feil: ${exception.message}")
+      .header(HttpHeaders.WARNING, "Det skjedde en ukjent feil: ${exception.message}}")
       .build<Any>()
   }
 
@@ -44,7 +44,7 @@ class DefaultRestControllerAdvice {
   @ResponseBody
   @ExceptionHandler(NoSuchElementException::class)
   fun handleNoSuchElementException(exception: java.util.NoSuchElementException): ResponseEntity<*> {
-    LOGGER.error("Fant ingen gyldig verdi: ${exception.message}", exception)
+    LOGGER.error("Fant ingen gyldig verdi: ${exception.message}}", exception)
     return ResponseEntity.status(HttpStatus.NO_CONTENT)
       .build<Any>()
   }
@@ -52,7 +52,7 @@ class DefaultRestControllerAdvice {
   @ResponseBody
   @ExceptionHandler(MaskinportenClientException::class)
   fun handleMaskinportenClientExcpetion(exception: MaskinportenClientException): ResponseEntity<*> {
-    LOGGER.error("Noe gikk galt ved kall til maskinporten: ${exception.message}", exception)
+    LOGGER.error("Noe gikk galt ved kall til maskinporten: ${exception.message}}", exception)
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
       .body(exception.message)
   }
