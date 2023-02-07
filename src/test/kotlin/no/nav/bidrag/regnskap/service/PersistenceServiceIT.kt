318998ed14214de057b4ef7fd31334fd1e96cadd
Simen Høston <simen.naper.hoston@webstep.no>
2023-02-07 14:24:46 +0100

    Legger til @DiretiesContext

--- src/test/kotlin/no/nav/bidrag/regnskap/service/PersistenceServiceIT.kt
+++ src/test/kotlin/no/nav/bidrag/regnskap/service/PersistenceServiceIT.kt
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.domain.Pageable
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
@@ -25,6 +26,7 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 
 @Transactional
+@DirtiesContext
 @ActiveProfiles("test")
 @EnableMockOAuth2Server
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
