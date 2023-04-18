adbabb7303e027337f3be38b68925befed6ec3be
Simen Høston <simen.naper.hoston@webstep.no>
2023-04-18 12:53:01 +0200

    Legger til controller for å hente behandlingsstatus på batch uid

---
+++ src/main/kotlin/no/nav/bidrag/regnskap/controller/BehandlingsstatusController.kt
@@ -0,0 +1,39 @@
+package no.nav.bidrag.regnskap.controller
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
+import no.nav.bidrag.regnskap.consumer.SkattConsumer
+import no.nav.security.token.support.core.api.Protected
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@Protected
+@Tag(name = "Behandlingsstatus")
+class BehandlingsstatusController(
+    private val skattConsumer: SkattConsumer
+) {
+
+    @GetMapping("/behandlingsstatus")
+    @Operation(
+        summary = "Sjekker behandlingsstatus for en Batch-uid.",
+        security = [SecurityRequirement(name = "bearer-key")]
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Aktive driftsavvik ble returnert.",
+                content = [Content()]
+            )
+        ]
+    )
+    fun hentBehandlingsstatus(batchUid: String): ResponseEntity<Any> {
+        return skattConsumer.sjekkBehandlingsstatus(batchUid)
+    }
+}
