ed74d09263347ccc0fb5db3e5fdd4d590bcc9807
Simen Høston <simen.naper.hoston@webstep.no>
2023-04-25 10:29:48 +0200

    Bedre logging ved feil på kafka

--- src/main/kotlin/no/nav/bidrag/regnskap/hendelse/kafka/vedtak/VedtakshendelseListener.kt
+++ src/main/kotlin/no/nav/bidrag/regnskap/hendelse/kafka/vedtak/VedtakshendelseListener.kt
@@ -5,6 +5,8 @@ import no.nav.bidrag.regnskap.SECURE_LOGGER
 import no.nav.bidrag.regnskap.service.VedtakshendelseService
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.KafkaHeaders
+import org.springframework.messaging.handler.annotation.Header
 
 private val LOGGER = LoggerFactory.getLogger(VedtakshendelseListener::class.java)
 
@@ -13,12 +15,21 @@ class VedtakshendelseListener(
 ) {
 
     @KafkaListener(groupId = "bidrag-regnskap", topics = ["\${TOPIC_VEDTAK}"])
-    fun lesHendelse(hendelse: String) {
+    fun lesHendelse(
+        hendelse: String,
+        @Header(KafkaHeaders.OFFSET) offset: Long,
+        @Header(KafkaHeaders.KEY) key: String,
+        @Header(KafkaHeaders.RECEIVED_PARTITION) partition: Int
+    ) {
         try {
             vedtakshendelseService.behandleHendelse(hendelse)
         } catch (e: JacksonException) {
-            LOGGER.error("Mapping av hendelse feilet! Se secure log for mer informasjon.")
-            SECURE_LOGGER.error("Mapping av hendelse feilet! Hendelse: $hendelse Feil: $e")
+            LOGGER.error(
+                "Mapping av hendelse feilet for kafkamelding med offsett: $offset, key: $key, recieved_partition: $partition!" +
+                        "\nSe secure log for mer informasjon."
+            )
+            SECURE_LOGGER.error("Mapping av hendelse feilet for kafkamelding med offsett: $offset, key: $key, recieved_partition: $partition! " +
+                    "\nFeil: $e \n\nHendelse: $hendelse")
         }
     }
 }
