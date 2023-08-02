41ac3da5f795049052ba4ad0554f01d2583ee36d
Simen Høston <simen.naper.hoston@webstep.no>
2023-08-02 15:46:14 +0200

    Helt ny config for å håntere aktørhendelser

--- src/main/kotlin/no/nav/bidrag/regnskap/config/KafkaConfiguration.kt
+++ src/main/kotlin/no/nav/bidrag/regnskap/config/KafkaConfiguration.kt
@@ -1,10 +1,19 @@
 package no.nav.bidrag.regnskap.config
 
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
 import no.nav.bidrag.regnskap.SECURE_LOGGER
+import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.config.SslConfigs
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.listener.ContainerProperties
 import org.springframework.kafka.listener.DefaultErrorHandler
 import org.springframework.kafka.support.ExponentialBackOffWithMaxRetries
 import org.springframework.util.backoff.ExponentialBackOff
@@ -12,7 +21,7 @@ import org.springframework.util.backoff.ExponentialBackOff
 private val LOGGER = LoggerFactory.getLogger(KafkaConfiguration::class.java)
 
 @Configuration
-class KafkaConfiguration {
+class KafkaConfiguration(val environment: Environment) {
 
     @Bean
     fun defaultErrorHandler(@Value("\${KAFKA_MAX_RETRY:-1}") maxRetry: Int): DefaultErrorHandler {
@@ -38,5 +47,55 @@ class KafkaConfiguration {
         }, backoffPolicy)
         errorHandler.setRetryListeners(KafkaRetryListener())
         return errorHandler
+    }
+
+    @Bean
+    fun kafkaAktorV2HendelseContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
+        factory.containerProperties.ackMode = ContainerProperties.AckMode.MANUAL_IMMEDIATE
+        factory.consumerFactory = DefaultKafkaConsumerFactory(consumerConfigsLatestAvro())
+        return factory
+    }
+
+    private fun consumerConfigsLatestAvro(): Map<String, Any> {
+        val kafkaBrokers = System.getenv("KAFKA_BROKERS") ?: "http://localhost:9092"
+        val schemaRegisty = System.getenv("KAFKA_SCHEMA_REGISTRY") ?: "http://localhost:9093"
+        val schemaRegistryUser = System.getenv("KAFKA_SCHEMA_REGISTRY_USER") ?: "mangler i pod"
+        val schemaRegistryPassword = System.getenv("KAFKA_SCHEMA_REGISTRY_PASSWORD") ?: "mangler i pod"
+        val consumerConfigs = mutableMapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to kafkaBrokers,
+            "schema.registry.url" to schemaRegisty,
+            "basic.auth.credentials.source" to "USER_INFO",
+            "basic.auth.user.info" to "$schemaRegistryUser:$schemaRegistryPassword",
+            "specific.avro.reader" to true,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to KafkaAvroDeserializer::class.java,
+            ConsumerConfig.CLIENT_ID_CONFIG to "consumer-familie-baks-mottak-2",
+            ConsumerConfig.MAX_POLL_RECORDS_CONFIG to "1",
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "latest",
+            ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to "false",
+        )
+
+        if (environment.activeProfiles.none { it.contains("local") || it.contains("h2") || it.contains("test") }) {
+            return consumerConfigs + securityConfig()
+        }
+        return consumerConfigs
+    }
+
+    private fun securityConfig(): Map<String, Any> {
+        val kafkaTruststorePath = System.getenv("KAFKA_TRUSTSTORE_PATH")
+        val kafkaCredstorePassword = System.getenv("KAFKA_CREDSTORE_PASSWORD")
+        val kafkaKeystorePath = System.getenv("KAFKA_KEYSTORE_PATH")
+        return mapOf(
+            CommonClientConfigs.SECURITY_PROTOCOL_CONFIG to "SSL",
+            SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG to "", // Disable server host name verification
+            SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG to "JKS",
+            SslConfigs.SSL_KEYSTORE_TYPE_CONFIG to "PKCS12",
+            SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG to kafkaTruststorePath,
+            SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG to kafkaCredstorePassword,
+            SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG to kafkaKeystorePath,
+            SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG to kafkaCredstorePassword,
+            SslConfigs.SSL_KEY_PASSWORD_CONFIG to kafkaCredstorePassword,
+        )
     }
 }
