d6a601a4392223d1e9d1edb576b5134bbfe5bdb0
Simen Høston <simen.naper.hoston@webstep.no>
2023-08-02 14:47:03 +0200

    Endrer til å behandle aktørV2 hendelser med avro aktør direkte

--- src/main/kotlin/no/nav/bidrag/regnskap/hendelse/kafka/pdl/AktorhendelserListener.kt
+++ src/main/kotlin/no/nav/bidrag/regnskap/hendelse/kafka/pdl/AktorhendelserListener.kt
@@ -4,7 +4,7 @@ import no.nav.bidrag.regnskap.SECURE_LOGGER
 import no.nav.bidrag.regnskap.pdl.aktor.v2.Aktor
 import no.nav.bidrag.regnskap.pdl.aktor.v2.Type
 import no.nav.bidrag.regnskap.service.AktorhendelseService
-import org.apache.avro.generic.GenericData
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
@@ -25,10 +25,15 @@ class AktorhendelserListener(
     @KafkaListener(
         groupId = "\${AKTOR_V2_GROUP_ID}",
         topics = ["\${TOPIC_PDL_AKTOR_V2}"],
-        properties = ["auto.offset.reset:latest"]
+        properties = [
+            "auto.offset.reset:latest",
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG + "=io.confluent.kafka.serializers.KafkaAvroDeserializer",
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG + "=org.apache.kafka.common.serialization.StringDeserializer",
+            "schema.registry.url=\${KAFKA_SCHEMA_REGISTRY}",
+        ]
     )
     fun lesHendelse(
-        consumerRecord: ConsumerRecord<String, GenericData.Record?>,
+        consumerRecord: ConsumerRecord<String, Aktor?>,
         @Header(KafkaHeaders.OFFSET) offset: Long,
         @Header(KafkaHeaders.RECEIVED_TOPIC) topic: String,
         @Header(KafkaHeaders.RECEIVED_PARTITION) partition: Int,
@@ -37,28 +42,20 @@ class AktorhendelserListener(
     ) {
         try {
             LOGGER.info("Behandler aktorhendelse med offset: $offset i consumergroup: $groupId for topic: $topic")
+            SECURE_LOGGER.info("Behandler aktorhendelse: $consumerRecord")
 
-            val genericRecord = GenericData.Record(Aktor.getClassSchema())
-            val identifikatorerField = genericRecord.get("identifikatorer")
+            val aktør = consumerRecord.value()
 
-            if (identifikatorerField is List<*>) {
-
-                LOGGER.info("Aktorhendelse: identifikatorliste: $identifikatorerField")
-                val aktor = identifikatorerField.filterIsInstance<GenericData.Record>()
-                LOGGER.info("Aktorhendelse: Aktor: $aktor")
-                for (identifikator in aktor) {
-                    val gjeldende = identifikator.get("gjeldende") as Boolean
-                    val type = identifikator.get("type") as Type
-                    LOGGER.info("Aktorhendelse: type: $type, gjeldende: $gjeldende")
-                    if (gjeldende && type == Type.FOLKEREGISTERIDENT) {
-                        val ident = identifikator.get("idnummer") as String
-                        aktorhendelseService.behandleAktoerHendelse(ident)
-                        SECURE_LOGGER.info("Oppdatert ident: $ident")
-                    }
-                }
+            aktør?.identifikatorer?.singleOrNull { ident ->
+                ident.type == Type.FOLKEREGISTERIDENT && ident.gjeldende
+            }?.also { folkeregisterident ->
+                aktorhendelseService.behandleAktoerHendelse(folkeregisterident.idnummer.toString())
+                LOGGER.info("Oppdatert indent ved hendelse for AktørV2 for offset: $offset")
             }
+
             acknowledgment.acknowledge()
             LOGGER.info("Behandlet hendelse med offset: $offset")
+            SECURE_LOGGER.info("Behandlet aktorhendelse for: $aktør")
         } catch (e: RuntimeException) {
             LOGGER.warn(
                 "Feil i prosessering av ident-hendelser med offsett: $offset, topic: $topic, recieved_partition: $partition, groupId: $groupId",
