7c9d7fa1d6c368ad2385d12440101bf0794ccb97
Simen Høston <simen.naper.hoston@webstep.no>
2023-08-16 11:25:30 +0200

    Ny versjon av bidrag-commons med caching av sjekk ident

--- src/main/kotlin/no/nav/bidrag/regnskap/util/IdentUtils.kt
+++ src/main/kotlin/no/nav/bidrag/regnskap/util/IdentUtils.kt
@@ -1,7 +1,6 @@
 package no.nav.bidrag.regnskap.util
 
 import no.nav.bidrag.commons.util.SjekkForNyIdent
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Component
 
 @Component
@@ -11,7 +10,6 @@ class IdentUtils {
         const val NAV_TSS_IDENT = "80000345435"
     }
 
-    @Cacheable(value = ["bidrag-regnskap_hentNyesteIdent_cache"], key = "#ident")
     fun hentNyesteIdent(@SjekkForNyIdent ident: String): String {
         return if (ident == "NAV") NAV_TSS_IDENT else ident
     }
