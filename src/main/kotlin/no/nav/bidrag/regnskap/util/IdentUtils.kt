29af1db23ff5dc87398a7868f0092116a9d3a792
Simen Høston <simen.naper.hoston@webstep.no>
2023-08-16 10:26:47 +0200

    Legger til cache på nyeste ident direkte i bidrag-regnskap for å slippe å kalle pointcut for hver ident flere ganger

--- src/main/kotlin/no/nav/bidrag/regnskap/util/IdentUtils.kt
+++ src/main/kotlin/no/nav/bidrag/regnskap/util/IdentUtils.kt
@@ -1,6 +1,7 @@
 package no.nav.bidrag.regnskap.util
 
 import no.nav.bidrag.commons.util.SjekkForNyIdent
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Component
 
 @Component
@@ -10,6 +11,7 @@ class IdentUtils {
         const val NAV_TSS_IDENT = "80000345435"
     }
 
+    @Cacheable(value = ["bidrag-regnskap_hentNyesteIdent_cache"], key = "#ident")
     fun hentNyesteIdent(@SjekkForNyIdent ident: String): String {
         return if (ident == "NAV") NAV_TSS_IDENT else ident
     }
