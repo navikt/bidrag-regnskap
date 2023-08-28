ca2a9a65dbb01fa3a3299659a97a07ebe015d2ef
Simen Høston <simen.naper.hoston@webstep.no>
2023-08-28 13:54:15 +0200

    Litt mer logging ved kjøring av avstemning

--- src/main/kotlin/no/nav/bidrag/regnskap/service/AvstemmingService.kt
+++ src/main/kotlin/no/nav/bidrag/regnskap/service/AvstemmingService.kt
@@ -1,6 +1,7 @@
 package no.nav.bidrag.regnskap.service
 
 import no.nav.bidrag.regnskap.fil.avstemning.AvstemmingsfilGenerator
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -11,7 +12,12 @@ class AvstemmingService(
     private val persistenceService: PersistenceService
 ) {
 
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(AvstemmingService::class.java)
+    }
+
     fun startAvstemming(dato: LocalDate) {
+        LOGGER.info("Starter avstemning for dato: $dato")
         avstemmingsfilGenerator.skrivAvstemmingsfil(
             persistenceService.hentAlleKonteringerForDato(dato).filter { it.behandlingsstatusOkTidspunkt != null },
             dato
@@ -19,6 +25,7 @@ class AvstemmingService(
     }
 
     fun startAvstemming(dato: LocalDate, fomTidspunkt: LocalDateTime, tomTidspunkt: LocalDateTime) {
+        LOGGER.info("Starter avstemning for dato: $dato for konteringer mellom $fomTidspunkt og $tomTidspunkt")
         val konteringer = persistenceService.hentAlleKonteringerForDato(dato, fomTidspunkt, tomTidspunkt).filter { it.behandlingsstatusOkTidspunkt != null }
         avstemmingsfilGenerator.skrivAvstemmingsfil(konteringer, dato)
     }
