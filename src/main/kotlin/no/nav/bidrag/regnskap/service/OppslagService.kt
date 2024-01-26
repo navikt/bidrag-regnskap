b4019f94f23d6bf8e989eecb4e29980185e59dd6
Simen Høston <simen.naper.hoston@webstep.no>
2024-01-26 19:06:27 +0100

    Legger til nytt endepunkt for å hente utsatte og feilede vedtak på saksnummer

--- src/main/kotlin/no/nav/bidrag/regnskap/service/OppslagService.kt
+++ src/main/kotlin/no/nav/bidrag/regnskap/service/OppslagService.kt
@@ -3,6 +3,9 @@ package no.nav.bidrag.regnskap.service
 import no.nav.bidrag.domene.enums.regnskap.Søknadstype
 import no.nav.bidrag.domene.enums.regnskap.Transaksjonskode
 import no.nav.bidrag.domene.enums.regnskap.Type
+import no.nav.bidrag.domene.enums.regnskap.behandlingsstatus.Batchstatus
+import no.nav.bidrag.domene.sak.Saksnummer
+import no.nav.bidrag.regnskap.consumer.SkattConsumer
 import no.nav.bidrag.regnskap.dto.oppdrag.KonteringResponse
 import no.nav.bidrag.regnskap.dto.oppdrag.OppdragResponse
 import no.nav.bidrag.regnskap.dto.oppdrag.OppdragsperiodeResponse
@@ -11,10 +14,13 @@ import no.nav.bidrag.regnskap.persistence.entity.Oppdrag
 import no.nav.bidrag.regnskap.persistence.entity.Oppdragsperiode
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 @Service
 class OppslagService(
     private val persistenceService: PersistenceService,
+    private val skattConsumer: SkattConsumer,
 ) {
 
     @Transactional
@@ -32,6 +38,66 @@ class OppslagService(
 
         return hentOppdragResponse(oppdrag)
     }
+
+    @Transactional
+    fun hentUtsatteOgFeiledeVedtakForSak(saksnummer: Saksnummer): UtsatteOgFeiledeVedtak {
+        val oppdrag = persistenceService.hentAlleOppdragPåSakId(saksnummer.verdi)
+        val now = LocalDate.now()
+
+        // Alle vedtak som har satt utsatt til dato etter dagens dato
+        val utsatteVedtak = oppdrag
+            .filter { it.utsattTilDato != null && it.utsattTilDato!!.isAfter(now) }
+            .flatMap { it.oppdragsperioder }
+            .distinctBy { it.vedtakId }
+            .map { UtsatteVedtak(it.vedtakId, it.oppdrag!!.utsattTilDato!!) }
+
+        // Alle vedtak som ikke har satt overføringstidspunkt, som vil si at det ikke har blitt sendt til oppdrag
+        val ikkeOversendteVedtak = oppdrag
+            .flatMap { it.oppdragsperioder }
+            .distinctBy { it.vedtakId }
+            .flatMap { it.konteringer }
+            .filter { it.overføringstidspunkt == null }
+            .map { IkkeOversendteVedtak(it.oppdragsperiode!!.vedtakId) }
+
+        // Alle vedtak som ikke har fått behandlingsstatus ok fra skatt. Her kalles skatt en gang for å sjekke om det kan godkjennes umiddelbart
+        val feiledeVedtak = oppdrag
+            .flatMap { it.oppdragsperioder }
+            .filter { oppdragsperiode -> oppdragsperiode.konteringer.any { it.behandlingsstatusOkTidspunkt == null } }
+            .flatMap { it.konteringer }
+            .filter { it.sisteReferansekode != null }
+            .distinctBy { it.sisteReferansekode }
+            .map {
+                val sjekkAvBehandlingsstatusResponse = skattConsumer.sjekkBehandlingsstatus(it.sisteReferansekode!!).body
+                var feilmelding: String? = null
+                if (sjekkAvBehandlingsstatusResponse?.batchStatus == Batchstatus.Done) {
+                    it.behandlingsstatusOkTidspunkt = LocalDateTime.now()
+                } else {
+                    feilmelding = sjekkAvBehandlingsstatusResponse?.konteringFeil?.firstOrNull()?.feilmelding
+                }
+                FeiledeVedtak(it.oppdragsperiode!!.vedtakId, feilmelding)
+            }.filter { it.feilmelding != null }
+
+        return UtsatteOgFeiledeVedtak(utsatteVedtak, ikkeOversendteVedtak, feiledeVedtak)
+    }
+
+    data class UtsatteOgFeiledeVedtak(
+        val utsatteVedtak: List<UtsatteVedtak>,
+        val ikkeOversendteVedtak: List<IkkeOversendteVedtak>,
+        val feiledeVedtak: List<FeiledeVedtak>,
+    )
+
+    data class IkkeOversendteVedtak(
+        val vedtakId: Int,
+    )
+
+    data class UtsatteVedtak(
+        val vedtakId: Int,
+        val utsattTil: LocalDate,
+    )
+    data class FeiledeVedtak(
+        val vedtakId: Int,
+        val feilmelding: String?,
+    )
 
     fun hentOppdragResponse(oppdrag: Oppdrag): OppdragResponse {
         return OppdragResponse(
